b7ec76d8bab0a49f0b97565d73a116e074e58a5b
Y Ramesh Rao <yrameshrao@gmail.com>
2019-04-28 05:39:39 +0200

    Fixing logging

--- src/api/serverConfiguration.js
+++ src/api/serverConfiguration.js
@@ -1,42 +1,58 @@
 const Hapi = require('hapi');
 const { inspect } = require('util');
 
-const server = new Hapi.Server({});
-const printMessage = message => console.log(inspect(message, false, Number.MAX_SAFE_INTEGER));
+const server = new Hapi.Server({ host: 'localhost', port: 3000 });
 
 async function init() {
-  printMessage('Initiliazing Server');
   server.route({
     method: 'GET',
     path: '/movies/user/{userId}/search',
-    handler: (request, headers) => {
-      printMessage('Request Handled');
+    options: {
+      log: {
+        collect: true,
+      },
+    },
+    handler: (request, handler) => {
+      request.log('info', 'Handling the request');
+      request.log('info', 'Handled the request');
       return 'Hello World !!';
     },
   });
 
+  server.log('info', 'Server Initialized');
   await server.initialize();
-  printMessage({ 'Server Initialize': server.info });
   return server;
 }
 
 async function start() {
+  server.log('info', 'Server Started');
   await server.start();
-  printMessage({ 'Server Started': server.info });
   return server;
 }
 
 async function stop() {
   await server.stop();
-  printMessage({ 'Server Stopped': server.info });
+  server.log('info', 'Server Stopped');
   return server;
 }
 
 process.on('unhandledRejection', err => {
-  printMessage(err);
   process.exit(1);
 });
 
+const printMessage = event => console.log(inspect(event, false, Number.MAX_SAFE_INTEGER));
+
+server.events.on({ name: 'request', channels: 'app' }, (request, event, tags) => {
+  printMessage(event);
+});
+
+server.events.on({ name: 'request', channels: 'internal' }, (request, event, tags) => {
+  printMessage(event);
+});
+
+server.events.on('log', (event, tags) => {
+  printMessage(event);
+});
 module.exports = {
   init,
   start,
