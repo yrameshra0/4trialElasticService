5531a5f0ceb33e249f8b658db3324cd9fa8b51be
Y Ramesh Rao <ramesh.rao@goeuro.com>
2019-05-20 15:34:20 +0200

    Clean up

--- idssearch.sh
+++ idssearch.sh
@@ -1,43 +1,4 @@
 #!/bin/sh
-req='{
-    "query": {
-        "ids": {
-            "values": [
-                "movieId:19995"
-            ]
-        }
-    },
-    "size": 0,
-    "aggs": {
-        "101": {
-            "filter": {
-                "term": {
-                    "originalLanguage": "de"
-                }
-            },
-            "aggs": {
-                "groupByIds": {
-                    "terms": {
-                        "field": "id",
-                        "include": [
-                            "movieId:19995"
-                        ]
-                    },
-                    "aggs": {
-                        "allResluts": {
-                            "top_hits": {
-                                "sort": [
-                                    {
-                                        "title": "asc"
-                                    }
-                                ]
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}'
+req=$(cat ./idsquery.json)
 echo "$req"
 curl -X GET "localhost:9200/movies/_search" -H 'Content-Type: application/json' -d "$req" | jq
