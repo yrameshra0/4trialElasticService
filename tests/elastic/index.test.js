58c92ba9359ba06357d3948c42e136a09cb9e93a
Y Ramesh Rao <yrameshrao@gmail.com>
2019-05-22 06:02:08 +0200

    Index resetting tested

--- tests/elastic/index.test.js
+++ tests/elastic/index.test.js
@@ -1,6 +1,7 @@
 const client = require('../../src/elastic');
 const elastic = require('../../src/elastic/_client');
 const sampleSearchBody = require('./sample_search_body');
+const indexMapping = require('./__snapshots__/indexMapping');
 
 const data = {
   type: 'somemovie',
@@ -39,5 +40,21 @@ describe('Elastic client and actions', () => {
 
     expect(searchSpy).toHaveBeenCalledTimes(1);
     expect(searchSpy).toBeCalledWith(sampleSearchBody);
+  });
+
+  test('Successful reseting index', async () => {
+    const index = 'movies';
+    const indiciesExitsSpy = jest.spyOn(elastic.indices, 'exists').mockResolvedValue(true);
+    const indiciesDeleteSpy = jest.spyOn(elastic.indices, 'delete').mockResolvedValue();
+    const indiciesCreateSpy = jest.spyOn(elastic.indices, 'create').mockResolvedValue();
+
+    await client.resetIndex();
+
+    expect(indiciesCreateSpy).toHaveBeenCalledTimes(1);
+    expect(indiciesExitsSpy).toHaveBeenCalledTimes(1);
+    expect(indiciesDeleteSpy).toHaveBeenCalledTimes(1);
+    expect(indiciesExitsSpy).toBeCalledWith({ index });
+    expect(indiciesDeleteSpy).toBeCalledWith({ index });
+    expect(indiciesCreateSpy).toBeCalledWith(indexMapping);
   });
 });
